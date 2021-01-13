aaa3efbb81528c71b0d58466cd3a5b56f642faf2
Rohit Pandit <rohit0311@outlook.com>
2021-01-13 17:28:28 +0530

    header-footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
 
 const App = () => {
-	return <h1 className='App'>welcome to proShop</h1>;
+	return (
+		<Fragment>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<h1 className='App'>welcome to proShop</h1>
+				</Container>
+			</main>
+			<Footer />
+		</Fragment>
+	);
 };
 
 export default App;
