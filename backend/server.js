80bd6a9fa5c05bcda0f0b3d15985d25ebb60ae90
Rohit Pandit <rohit0311@outlook.com>
2021-01-14 23:32:47 +0530

    basic express server

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const products = require('./data/products');
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Api is running...');
+});
+
+app.get('/api/products', (req, res) => {
+	res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+	const product = products.find((p) => p._id === req.params.id);
+	res.json(product);
+});
+
+app.get('*', (req, res) => {
+	res.send(' 404! not Found!');
+});
+
+app.listen(5000, () => console.log('server running at 5000 port'));
