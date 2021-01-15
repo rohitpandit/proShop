9238e7b15c8ed734c9ef3e374d4909b3c617e45f
Rohit Pandit <rohit0311@outlook.com>
2021-01-15 17:25:01 +0530

    productScreen with reducer

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,8 +3,12 @@ import {
 	PRODUCT_LIST_REQUEST,
 	PRODUCT_LIST_SUCCESS,
 	PRODUCT_LIST_FAIL,
+	PRODUCT_DETAILS_SUCCESS,
+	PRODUCT_DETAILS_FAIL,
+	PRODUCT_DETAILS_REQUEST,
 } from '../constants/productConstants';
 
+//list of products
 export const listProducts = () => async (dispatch) => {
 	try {
 		dispatch({ type: PRODUCT_LIST_REQUEST });
@@ -14,6 +18,24 @@ export const listProducts = () => async (dispatch) => {
 	} catch (error) {
 		dispatch({
 			type: PRODUCT_LIST_FAIL,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
+	}
+};
+
+//single product details
+export const listProductDetails = (id) => async (dispatch) => {
+	try {
+		dispatch({ type: PRODUCT_DETAILS_REQUEST });
+
+		const { data } = await axios.get(`/api/products/${id}`);
+		dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+	} catch (error) {
+		dispatch({
+			type: PRODUCT_DETAILS_FAIL,
 			payload:
 				error.response && error.response.data.message
 					? error.response.data.message
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,9 +1,13 @@
 import {
+	PRODUCT_DETAILS_FAIL,
+	PRODUCT_DETAILS_REQUEST,
+	PRODUCT_DETAILS_SUCCESS,
 	PRODUCT_LIST_FAIL,
 	PRODUCT_LIST_REQUEST,
 	PRODUCT_LIST_SUCCESS,
 } from '../constants/productConstants';
 
+//list of products
 export const productListReducer = (state = { products: [] }, action) => {
 	switch (action.type) {
 		case PRODUCT_LIST_REQUEST:
@@ -17,6 +21,32 @@ export const productListReducer = (state = { products: [] }, action) => {
 				products: action.payload,
 			};
 		case PRODUCT_LIST_FAIL:
+			return {
+				loading: false,
+				error: action.payload,
+			};
+		default:
+			return state;
+	}
+};
+
+//single product
+export const productDetailsReducer = (
+	state = { product: { reviews: [] } },
+	action
+) => {
+	switch (action.type) {
+		case PRODUCT_DETAILS_REQUEST:
+			return {
+				loading: true,
+				...state,
+			};
+		case PRODUCT_DETAILS_SUCCESS:
+			return {
+				loading: false,
+				product: action.payload,
+			};
+		case PRODUCT_DETAILS_FAIL:
 			return {
 				loading: false,
 				error: action.payload,
