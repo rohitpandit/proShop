9036a8ecfd2735faf3c0a8659b6658a4a39bc07e
Rohit Pandit <rohit0311@outlook.com>
2021-01-15 10:51:45 +0530

    environment variables and some config

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const products = require('./data/products');
+
+dotenv.config();
 
 const app = express();
 
@@ -20,4 +23,7 @@ app.get('*', (req, res) => {
 	res.send(' 404! not Found!');
 });
 
-app.listen(5000, () => console.log('server running at 5000 port'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+	console.log(`server running in ${process.env.NODE_ENV} mode at port:${PORT} `)
+);
