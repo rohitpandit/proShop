38d1e2434fd67b7d79198ec8736f50e163c1ba60
Rohit Pandit <rohit0311@outlook.com>
2021-01-18 21:24:00 +0530

    shipping address page and shipping address

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 const App = () => {
 	return (
@@ -17,11 +18,12 @@ const App = () => {
 				<Header />
 				<main className='py-3'>
 					<Container>
-						<Route exact path='/profile' component={ProfileScreen} />
 						<Route exact path='/login' component={LoginScreen} />
 						<Route exact path='/register' component={RegisterScreen} />
+						<Route exact path='/profile' component={ProfileScreen} />
 						<Route exact path='/product/:id' component={ProductScreen} />
 						<Route exact path='/cart/:id?' component={CartScreen} />
+						<Route exact path='/shipping' component={ShippingScreen} />
 						<Route exact path='/' component={HomeScreen} />
 					</Container>
 				</main>
