ec0899cef03563c22ce2dc191a3e607ff2327157
Rohit Pandit <rohit0311@outlook.com>
2021-01-18 14:09:24 +0530

    redux setup for login

---
+++ frontend/src/actions/userAction.js
@@ -0,0 +1,44 @@
+import {
+	USER_LOGIN_FAIL,
+	USER_LOGIN_REQUEST,
+	USER_LOGIN_SUCCESS,
+} from '../constants/userConstants.js';
+import axios from 'axios';
+
+export const login = (email, password) => async (dispach) => {
+	try {
+		dispach({
+			type: USER_LOGIN_REQUEST,
+		});
+
+		const config = {
+			headers: {
+				'content-type': 'application/json',
+			},
+		};
+
+		const { data } = await axios.post(
+			'/api/users/login',
+			{
+				email,
+				password,
+			},
+			config
+		);
+
+		dispach({
+			type: USER_LOGIN_SUCCESS,
+			payload: data,
+		});
+
+		localStorage.setItem('userInfo', JSON.stringify(data));
+	} catch (error) {
+		dispatch({
+			type: USER_LOGIN_FAIL,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
+	}
+};
